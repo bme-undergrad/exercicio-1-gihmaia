186b3e2642d6599c64887ba4a71f706f0cb4f2c5
gihmaia <giovannamaiamachado.imp@gmail.com>
2025-09-01 14:35:39 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,15 +1,24 @@
-function t = exercicio1(func,x0)
+function t_final = exercicio1(func, func_d, x0)
 
-% nao alterar: inicio
-es = 1;
-imax = 20;
-% nao alterar: fim
+    es = 0.01;
+    imax = 20;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+    t = zeros(imax, 1);
+    erro = zeros(imax, 1);
 
-% digite seu codigo aqui
-t = 0; % alterar
+    t(1) = x0;
+    for i = 1:imax-1
+        t(i+1) = t(i) - func(t(i)) / func_d(t(i));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if t(i+1) ~= 0
+            erro(i+1) = abs((t(i+1) - t(i)) / t(i+1)) * 100;
+        end
 
-endfunction
+        if erro(i+1) < es
+            t_final = t(i+1);
+            return
+        end
+    end
+
+    t_final = t(imax);
+end
