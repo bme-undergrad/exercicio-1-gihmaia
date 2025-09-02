37025cf2bca8eb9323ce714ac9304075538beafb
gihmaia <giovannamaiamachado.imp@gmail.com>
2025-09-02 14:21:54 -0300

    Update exercicio1.m

--- exercicio1.m
+++ exercicio1.m
@@ -1,24 +1,26 @@
-function t = exercicio1(func,func_d,x0)
+function t = exercicio1(func,func_d, x0)
 
 % nao alterar: inicio
-es = 0.01;
+es = 1;
 imax = 20;
 % nao alterar: fim
 
+t = zeros(imax,1)
+t(1) = x0
 
-t_roots = zeros(imax,1);
-t_roots(1) = x0;
-
-for ii = 1:length(t_roots)-1
+for ii = 1:length(t)-1
+  ii
   if ii ~= 1
-    erro(ii) = abs((t_roots(ii)-t_roots(ii-1))/t_roots(ii))
+    erro(ii) = abs((t(ii)-t(ii-1))/t(ii))
     if erro(ii) < es
       break
     endif
   endif
-    t_roots(ii+1) = t_roots(ii) - func(t_roots(ii))/func_d(t_roots(ii))
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii))
 endfor
-t = t_roots(ii); 
+
+disp('O valor é: ')
+t = t(ii)
 
 
 endfunction
